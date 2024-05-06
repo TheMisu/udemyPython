1838ec0963898cfe71ea69a2e8c47cd35328cff9
Mihai Paun <email.mihu@gmail.com>
2024-05-06 15:43:13 +0200

    Solved an issue with the formatting of individual amounts that only have ONE decimal. The program now always prints 2 decimals

--- 02_tip_calculator.py
+++ 02_tip_calculator.py
@@ -13,7 +13,10 @@ people = int(input("How many people to split the bill?\n"))
 
 # computing the price per person and rounding it to 2 decimals
 individual_price = round((bill / people) * tip_final, 2)
+# formatting the individual amount so that it ALWAYS prints 2 decimals
+# ex. %150, 12%, 5 => $33.6 instead of $33.60
+final_amount = "{:.2f}".format(individual_price)
 
 # printing the result
-print(f"Each person should pay: ${individual_price}")
+print(f"Each person should pay: ${final_amount}")
 
