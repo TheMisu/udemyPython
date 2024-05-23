920a861792af3a20d8b097706c559a53ab2bdc11
Mihai Paun <email.mihu@gmail.com>
2024-05-23 18:07:17 +0200

    Changed the food's shape

--- 20 Snake Game/food.py
+++ 20 Snake Game/food.py
@@ -7,9 +7,9 @@ class Food(Turtle):
         Constructor for the Food class
         """
         super().__init__()
-        self.shape("square")
+        self.shape("circle")
         self.penup()
-        # self.shapesize(stretch_len=0.5, stretch_wid=0.5) # 20 units * 0.5 (makes it smaller)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5) # 20 units * 0.5 (makes it smaller)
         self.color("red")
         self.speed("fastest")
         self.change_pos()
@@ -20,5 +20,5 @@ class Food(Turtle):
         Method that changes the food's position on screen
         """
         random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
+        random_y = random.randint(-280, 250)
         self.goto(random_x, random_y)        
